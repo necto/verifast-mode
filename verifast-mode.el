bdf95f50950b4e05bd22fc17f0f8096116e7d6a0
Necto <necto.ne@gmail.com>
2017-01-29 14:09:39 +0100

    ditch some more rust specifics

--- verifast-mode.el
+++ verifast-mode.el
@@ -1357,7 +1357,7 @@ This is written mainly to be used as `end-of-defun-function' for Verifast."
   (add-hook 'before-save-hook 'verifast--before-save-hook nil t))
 
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . verifast-mode))
+(add-to-list 'auto-mode-alist '("\\.gh\\'" . verifast-mode))
 
 (defun verifast-mode-reload ()
   (interactive)
@@ -1376,28 +1376,6 @@ This is written mainly to be used as `end-of-defun-function' for Verifast."
     (if (fboundp 'font-lock-ensure)
         (font-lock-ensure)
       (font-lock-fontify-region (point-min) (point-max)))))
-
-(defun verifast-promote-module-into-dir ()
-  "Promote the module file visited by the current buffer into its own directory.
-
-For example, if the current buffer is visiting the file `foo.rs',
-then this function creates the directory `foo' and renames the
-file to `foo/mod.rs'.  The current buffer will be updated to
-visit the new file."
-  (interactive)
-  (let ((filename (buffer-file-name)))
-    (if (not filename)
-        (message "Buffer is not visiting a file.")
-      (if (string-equal (file-name-nondirectory filename) "mod.rs")
-          (message "Won't promote a module file already named mod.rs.")
-        (let* ((basename (file-name-sans-extension
-                          (file-name-nondirectory filename)))
-               (mod-dir (file-name-as-directory
-                         (concat (file-name-directory filename) basename)))
-               (new-name (concat mod-dir "mod.rs")))
-          (mkdir mod-dir t)
-          (rename-file filename new-name 1)
-          (set-visited-file-name new-name))))))
 
 (provide 'verifast-mode)
 
